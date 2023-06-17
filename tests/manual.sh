e4a18e824dccea8781054e21dec89ef0ab0a56d8
dbear496 <70439542+dbear496@users.noreply.github.com>
2023-06-17 15:02:20 -0400

    add warning to tests/manual.sh about seed update

--- tests/manual.sh
+++ tests/manual.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 tag="test tag"
+expseed="test seed"
 exppw="wJgh0hKXLB/K4YyZWgScRLw5YbAbxdFEUvY9DDGgx4U="
+
+if [[ $seed != $expseed ]]; then
+  echo "the test seed is '$seed', but this test uses an old seed ('$expseed')"
+  echo "please update this test to use the new seed"
+  exit 1
+fi
 
 genpw=$(./genpass.sh --home $homedir --output >(cat) < <(echo -e "${tag}\ny\n${masterpw}") >/dev/null)
 
